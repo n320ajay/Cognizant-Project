6b6b3d25d7bea231d84656fe197fe55b99f5fd1e
n320ajay <47206099+n320ajay@users.noreply.github.com>
2024-07-01 12:02:32 +0530

    Update CollateralWebPortalApplicationTests.java

--- Collateral loans Risk assessment/collateral-web-portal/src/test/java/com/cts/training/collateralwebportal/CollateralWebPortalApplicationTests.java
+++ Collateral loans Risk assessment/collateral-web-portal/src/test/java/com/cts/training/collateralwebportal/CollateralWebPortalApplicationTests.java
@@ -2,7 +2,7 @@ package com.cts.training.collateralwebportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//updating changes testing
 @SpringBootTest
 class CollateralWebPortalApplicationTests {
 
